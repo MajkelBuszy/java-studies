d5e8a20bcf766f2bea446e083974aaf11c25435b
MajkelBuszy <swierzewski.biz@gmail.com>
2023-04-30 09:21:56 +0200

    added vetClinic class

--- lesson_4/abstrac_classes/src/Main.java
+++ lesson_4/abstrac_classes/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Cat someCat = new Cat("Nice cat", 10);
-        System.out.println("Pet: " + someCat.name + " is" + someCat.age + " years old");
+        System.out.println("Pet: " + someCat.name + " is " + someCat.age + " years old");
         Dog someDog = new Dog("some dog", 20);
-        System.out.println("Pet: " + someDog.name + " is" + someDog.age + " years old");
+        System.out.println("Pet: " + someDog.name + " is " + someDog.age + " years old");
         Person somePerson = new Person("Edziu");
         somePerson.setPet(someCat);
     }
---
+++ lesson_4/abstrac_classes/src/VetClinic.java
@@ -0,0 +1,4 @@
+public class VetClinic {
+    String name;
+    Pet pet;
+}
